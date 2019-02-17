570e0ae34eebc5d0c685f34c77491055b648d3b6
Mohammed Alotaibi <imohammed.mg@gmail.com>
2019-02-17 12:56:17 +0300

    Atallah Alotaibi&Mohammed Alotaibi

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Restaurant
-from .forms import RestaurantForm, SignupForm, SigninForm
+from .models import Restaurant, Item
+from .forms import RestaurantForm, SignupForm, SigninForm, ItemForm
 from django.contrib.auth import login, authenticate, logout
 
 def signup(request):
@@ -50,27 +50,41 @@ def restaurant_list(request):
 
 
 def restaurant_detail(request, restaurant_id):
+    restaurant_obj= Restaurant.objects.get(id=restaurant_id)
+    items = restaurant_obj.items.all()
     context = {
-        "restaurant": Restaurant.objects.get(id=restaurant_id)
+        "restaurant": restaurant_obj,
+        "items": items,
     }
     return render(request, 'detail.html', context)
 
 def restaurant_create(request):
     form = RestaurantForm()
     if request.method == "POST":
-        form = RestaurantForm(request.POST, request.FILES)
+        form = RestaurantForm(request.POST, request.FILES or None)
         if form.is_valid():
-            form.save()
+            restaurant = form.save(commit=False)
+            restaurant.owner = request.user
+            restaurant.save()
             return redirect('restaurant-list')
     context = {
         "form":form,
     }
     return render(request, 'create.html', context)
 
-def item_create(request):
-
+def item_create(request, restaurant_id):
+    restaurant_obj = Restaurant.objects.get(id=restaurant_id)
+    form = ItemForm()
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid(): 
+            item = form.save(commit=False)
+            item.restaurant = restaurant_obj
+            form.save()
+            return redirect('restaurant-detail', restaurant_id=restaurant_obj.id)
     context = {
-        
+        "form":form,
+        "restaurant":restaurant_obj
     }
     return render(request, 'item_create.html', context)
 
